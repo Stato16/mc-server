5db3b7915d0bad1f7c0b497243015042a9830e34
Stato16 <68954640+Stato16@users.noreply.github.com>
2020-10-28 10:24:33 +0000

    latest files

---
+++ run.bat
@@ -0,0 +1,8 @@
+@echo OFF
+:loop
+java -Xmx1024M -Xms1024M -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar paper.jar nogui
+echo Press control+C to exit the server!
+timeout 10
+echo (%time%) Restarting!
+goto loop
+::java -Xmx1024M -Xms1024M -jar paper.jar nogui
